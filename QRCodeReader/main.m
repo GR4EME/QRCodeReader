310b35f3808fa6633fe0fa120bd8403bcfd99679
Graeme Littlewood <graemelittlewood@graemes-mbp.lan>
2013-11-17 09:24:50 +0000

    Initial Commit

---
+++ QRCodeReader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QRCodeReader
+//
+//  Created by Graeme Littlewood on 17/11/2013.
+//  Copyright (c) 2013 Graeme Littlewood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
